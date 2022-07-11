1b3985fb25a7490d98923686889c94fd9495e36e
Damjan15 <stamenovdamjan20@gmail.com>
2022-07-11 15:36:32 -0700

    setup: installed necessary dependencies, files and folder structures setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { ExerciseDetail, Home } from './pages';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <h1>Hello World 👋</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
   );
 }
 
