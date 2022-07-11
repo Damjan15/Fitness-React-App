d0d18270f6b52c3ca3a1080c809b09cbdbb00605
Damjan15 <stamenovdamjan20@gmail.com>
2022-07-11 15:52:28 -0700

    feature: navbar added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Box } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { ExerciseDetail, Home } from './pages';
+import { Footer, Navbar } from "./components"
 import './App.css';
 
 
 function App() {
   return (
-    <Box>
-      <h1>Hello World 👋</h1>
+    <Box width="400px" sx={
+      { width: { xl: "1488px"}}
+    } m="auto">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/exercise/:id' element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
