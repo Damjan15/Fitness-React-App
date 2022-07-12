f52d366e8ca1a4ea49ccaa1066f7d24aba1b6795
Damjan15 <stamenovdamjan20@gmail.com>
2022-07-12 13:34:29 -0700

    feature: pagination added, rendering exercises depending on the selected category, showing all the exercises if there are no categories selected

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { Box } from "@mui/material"
-import { HeroBanner, SearchExercises } from "../components"
+import { Exercises, HeroBanner, SearchExercises } from "../components"
 
 const Home = () => {
   const [ exercises, setExercises ] = useState([])
   const [ bodyPart, setBodyPart ] = useState("all");
+
+  console.log(bodyPart)
 
   return (
     <Box>
@@ -13,6 +15,7 @@ const Home = () => {
       {/* SearchExercises */}
       <SearchExercises exercises={exercises} setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       {/* Exercises */}
+      <Exercises exercises={exercises} bodyPart={bodyPart} setExercises={setExercises} />
     </Box>
   )
 }
